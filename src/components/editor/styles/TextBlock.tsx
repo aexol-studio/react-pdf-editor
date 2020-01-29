d08bc7d1808bca89027a22f5c52053c93edf57cf
ls <krzysiek@aexol.com>
2020-01-29 14:01:27 +0100

    fixed textarea font

--- src/components/editor/styles/TextBlock.tsx
+++ src/components/editor/styles/TextBlock.tsx
@@ -2,14 +2,15 @@ import { style } from "typestyle";
 import { Colors } from "../../styles/Colors";
 
 export const Main = style({
-  $debugName:"TextBlockMain",
+  $debugName: "TextBlockMain",
   border: 0,
   width: "100%",
   background: Colors.White,
-  padding: 10
+  padding: 10,
+  fontFamily: "Fira Sans"
 });
 export const styleLabel = style({
-  $debugName:"TextBlockStyleLabel",
+  $debugName: "TextBlockStyleLabel",
   fontSize: 10,
   color: Colors.Black,
   padding: 5,
