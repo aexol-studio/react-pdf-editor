a605955b78bd25b9cac1e52a19c03426759a8a89
ls <krzysiek@aexol.com>
2020-01-29 15:33:10 +0100

    first try text area

--- src/components/editor/TextBlockComponent.tsx
+++ src/components/editor/TextBlockComponent.tsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useRef } from "react";
 import { PartialObjects } from "../../graphql-zeus";
 import * as styles from "./styles/TextBlock";
+
 export interface TextBlockComponentProps {
   textBlock: PartialObjects["TextBlock"];
   onChange: () => void;
 }
 
+// export interface TextBlockComonentState{
+//   height: number;
+//   rows: number;
+//   minRows: number;
+//   maxRows: number;
+// }
+
 export const TextBlockComponent = ({
   onChange,
   textBlock
-}: TextBlockComponentProps) => {
+
+}:  TextBlockComponentProps) => {
   const inref = useRef(null);
   useEffect(() => {}, [inref]);
+
+
+  // https://codepen.io/liborgabrhel/pen/eyzwOx
+
   return (
     <textarea
       ref={inref}
