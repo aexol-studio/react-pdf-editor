dd7475c72912685dc20435cd76d293121030aef3
Krzysiek <krzysiek@aexol.com>
2020-03-25 08:43:14 +0100

    added reactPDFStyleValidator

---
+++ src/screens/ReactPDFStyleValidator.tsx
@@ -0,0 +1,67 @@
+import ReactPDF from "@react-pdf/renderer";
+
+/**
+ * If style does not validate replace incorrect values with undefined to keep the up running and throw console.error only
+ */
+export const ReactPDFStyleValidator = (
+  style: ReactPDF.Style
+): ReactPDF.Style => {
+  const copyStyle: ReactPDF.Style = {};
+  const validateNumberValue = (v: string) => {
+    const validatedNumber = parseFloat(v);
+    if (!isNaN(validatedNumber)) {
+      return v;
+    }
+    return undefined;
+  };
+  const numberKeys: Array<keyof ReactPDF.Style> = [
+    "borderBottomLeftRadius",
+    "borderBottomRightRadius",
+    "borderBottomWidth",
+    "borderRadius",
+    "borderRightWidth",
+    "borderTopLeftRadius",
+    "borderTopRightRadius",
+    "borderTopWidth",
+    "borderWidth",
+    "bottom",
+    "fontSize",
+    "height",
+    "left",
+    "letterSpacing",
+    "lineHeight",
+    "margin",
+    "marginBottom",
+    "marginHorizontal",
+    "marginLeft",
+    "marginRight",
+    "marginTop",
+    "marginVertical",
+    "maxHeight",
+    "minHeight",
+    "minWidth",
+    "maxWidth",
+    "opacity",
+    "order",
+    "padding",
+    "paddingBottom",
+    "paddingHorizontal",
+    "paddingLeft",
+    "paddingRight",
+    "paddingTop",
+    "paddingVertical",
+    "right",
+    "width"
+  ];
+  numberKeys.forEach(k => {
+    if (style[k]) {
+      const v = validateNumberValue(style[k]);
+      if (v) {
+        copyStyle[k] = v;
+      } else {
+        console.error(`Key "${k}" has the incorrect value`);
+      }
+    }
+  });
+  return copyStyle;
+};
