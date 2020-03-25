3cf8079a71a2038e1e1d0fd2f5a3cebe51a043d9
Krzysiek <krzysiek@aexol.com>
2020-03-25 13:26:57 +0100

    added coments in reactValidator  code

--- src/screens/ReactPDFStyleValidator.tsx
+++ src/screens/ReactPDFStyleValidator.tsx
@@ -7,14 +7,37 @@ export const ReactPDFStyleValidator = (
   style: ReactPDF.Style
 ): ReactPDF.Style => {
   const copyStyle: ReactPDF.Style = {};
+    // metoda copyStyle która trzyma ReactPDF.Style która jest w formie obiektu 
+//     ReactPDF.Style = { paddingLeft?: number | string }
   const validateNumberValue = (v: string) => {
+  	// zmiena  validateNumberValue pobiera string v z jakiego inputa i tworzy funkcję 
     const validatedNumber = parseFloat(v);
+    	// która w zmiennej validatedNumber zwaraca przez parseFloat zawsze licze 
+    
     if (!isNaN(validatedNumber)) {
+    
+    // jeśli zmienna validatedNumber jest NUMEREM bo jest negacja ! to zwraca tego stringa(NUMER)
+    
       return v;
+
+      // v jest Numerem
     }
+    
+   // inaczej wypierdala go jako NIEZNANY undefined
+   
     return undefined;
   };
+  
+  
+  // czu na górze była pierwsza walidacja 
+
+// v w tuym momencie jest numerem bylo pobrane jak o input zmienione na numerek i zwrócone jako v przez returniaka
+  
+  
   const numberKeys: Array<keyof ReactPDF.Style> = [
+  
+  // zmienna co trzyma w sobie wszystkie dostępne style z ReactPDF w tablicy o nazwie numberKeys
+  
     "borderBottomLeftRadius",
     "borderBottomRightRadius",
     "borderBottomWidth",
@@ -53,15 +76,41 @@ export const ReactPDFStyleValidator = (
     "right",
     "width"
   ];
+ 
   numberKeys.forEach(k => {
-    if (style[k]) {
-      const v = validateNumberValue(style[k]);
-      if (v) {
+  // dla każdego elementu tablicy numberKeys
+  
+if (style[k]) {
+//   jeśli style(k) występuje to [co to kurwa za jebane gówno ]
+			const v = validateNumberValue(style[k]);
+  		// zmienna v będzie miała 
+      if (v)
+      // jeśli numer V po wstepnej walidacji występuje to k - które jest elementem 
+      // tablicy np. paddingLeft i v które jest numrem. 
+
+
+
+      {
         copyStyle[k] = v;
+
+      // pusty obiekt React.Style
+      // ostatenicze będzie wyglądal tak
+      // "paddingLeft = 13 "
+
       } else {
+      
         console.error(`Key "${k}" has the incorrect value`);
+        // albo wykonsoluje błąd 
+
       }
     }
   });
+
   return copyStyle;
+
+  // na koniec zostanie zwrócone copystyle co będzie wyglądało jak
+
+  //copyStyle: ReactPDF.Style = {paddingLeft: 13};
+
+  
 };
--- src/topmenu/items/InputFontSize.tsx
+++ src/topmenu/items/InputFontSize.tsx
@@ -3,5 +3,6 @@ import { MenuItemType, SmallInputMenuItem } from "../models";
 export const InputFontSize: SmallInputMenuItem = {
   name: "fontSize",
   itemType: MenuItemType.SmallInput,
-  change: (e: string) => ({ fontSize: e })
+  change: (e: string) => (
+    {fontSize: e })
 };
