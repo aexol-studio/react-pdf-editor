b49af0483569eb6a530db8673787947204c3d97f
ls <krzysiek@aexol.com>
2020-09-08 10:12:32 +0200

    pusshing objct to column array

--- src/components/editor/TableShowComponent.tsx
+++ src/components/editor/TableShowComponent.tsx
@@ -21,7 +21,7 @@ const ResizableTitle = (props: any) => {
 };
 
 interface RecordType {
-  a: string;
+  a?: string;
   b?: string;
   c?: string;
   d?: number;
@@ -112,23 +112,32 @@ class Demo extends React.Component<{}, DemoState> {
   ) => {
     this.setState(({ columns }) => {
       const nextColumns = [...columns];
+      // spread operator - tworzy kopię tablicy columns - kopia tablicy ma nazwę nextColumns
       nextColumns[index] = {
+        // element tablicy nextColumns ???
         ...nextColumns[index],
+        // ????
         width: size.width,
+        // ???
       };
       return { columns: nextColumns };
+
+      // tutaj jest przypisanie columns do
     });
   };
 
   render() {
-    const columns = this.state.columns.map((col, index) => ({
+    const headerFunc = (col: any, index: number) => ({
       ...col,
       onHeaderCell: (column: ColumnType<RecordType>) =>
         ({
           width: column.width,
           onResize: this.handleResize(index),
         } as any),
-    }));
+    });
+    const columns = this.state.columns.map((col, index) =>
+      headerFunc(col, index)
+    );
 
     return (
       <div>
@@ -144,24 +153,25 @@ class Demo extends React.Component<{}, DemoState> {
               className={styles.ControlAddRow}
               focusable={"false"}
               viewBox={"0 0 24 24"}
-              // onClick={() => {
-              //   if (tableBlock.widths!.length === 0) {
-              //     tableBlock.widths!.push({
-              //       S: "*",
-              //     });
-              //     tableBlock.widths! = normalizeWidths(tableBlock.widths!);
-              //   }
-              //   tableBlock.rows!.push({
-              //     __typename: "Row",
-              //     columns:
-              //       tableBlock.rows!.length > 0
-              //         ? tableBlock.rows![
-              //             tableBlock.rows!.length - 1
-              //           ].columns!.map(() => emptyColumn())
-              //         : [emptyColumn()],
-              //   });
-              //   onChange();
-              // }}
+              onClick={() => {
+                // if (tableBlock.widths!.length === 0) {
+                //   tableBlock.widths!.push({
+                //     S: "*",
+                //   });
+                //   tableBlock.widths! = normalizeWidths(tableBlock.widths!);
+                // }
+                // tableBlock.rows!.push({
+                //   __typename: "Row",
+                //   columns:
+                //     tableBlock.rows!.length > 0
+                //       ? tableBlock.rows![
+                //           tableBlock.rows!.length - 1
+                //         ].columns!.map(() => emptyColumn())
+                //       : [emptyColumn()],
+                // });
+                // onChange();
+                console.log("add row");
+              }}
             >
               <path d="M20,5H4C2.9,5,2,5.9,2,7v2v1.5v3V15v2c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2v-2v-1.5v-3V9V7C22,5.9,21.1,5,20,5z M16,6.5h4  c0.3,0,0.5,0.2,0.5,0.5v2H16V6.5z M9.5,6.5h5V9h-5V6.5z M3.5,7c0-0.3,0.2-0.5,0.5-0.5h4V9H3.5V7z M8,17.5H4c-0.3,0-0.5-0.2-0.5-0.5  v-2H8V17.5z M14.5,17.5h-5V15h5V17.5z M20.5,17c0,0.3-0.2,0.5-0.5,0.5h-4V15h4.5V17z"></path>
             </svg>
@@ -172,16 +182,26 @@ class Demo extends React.Component<{}, DemoState> {
               className={styles.ControlAddColumn}
               focusable={"false"}
               viewBox={"0 0 24 24"}
-              // onClick={() => {
-              //   tableBlock.rows!.forEach((row) => {
-              //     row.columns!.push(emptyColumn());
-              //   });
-              //   tableBlock.widths!.push({
-              //     S: "100%",
-              //   });
-              //   tableBlock.widths! = normalizeWidths(tableBlock.widths!);
-              //   onChange();
-              // }}
+              onClick={() => {
+                columns.push(
+                  headerFunc(
+                    {
+                      dataIndex: "a",
+                      key: "a",
+                      width: 150,
+                      render() {
+                        return <input width="100%" type="text"></input>;
+                      },
+                    },
+                    columns.length
+                  )
+                );
+
+                //w tym momencie columns ma w sobie jeszcze metodę onHeaderCell no i jak chce pusznąć nowy obiekt do tablicy columns to co musze zrobić?
+
+                console.log("cokolwiek");
+                console.log(columns);
+              }}
             >
               <path d="M20,5H4C2.9,5,2,5.9,2,7v10c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V7C22,5.9,21.1,5,20,5z M8,17.5H4c-0.3,0-0.5-0.2-0.5-0.4  c0,0,0,0,0,0V17v-2H8V17.5z M8,13.5H3.5v-3H8V13.5z M8,9H3.5V7c0-0.3,0.2-0.5,0.4-0.5c0,0,0,0,0,0H8V9z M20.5,17  c0,0.3-0.2,0.5-0.4,0.5c0,0,0,0,0,0H16V15h4.5V17z M20.5,13.5H16v-3h4.5V13.5z M20.5,9H16V6.5h4c0.3,0,0.5,0.2,0.5,0.4c0,0,0,0,0,0  V9z"></path>
             </svg>
